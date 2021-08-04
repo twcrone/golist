c4d3a76fe1c9011b30cdcf0c401df0f2d1e2dd81
Todd Crone <twcrone@gmail.com>
2021-08-03 21:43:49 -0400

    Use CORS default

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 )
 
 type Item struct {
@@ -61,17 +60,8 @@ func main() {
 	}
 
 	router := gin.New()
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://foo.com"},
-		AllowMethods:     []string{"PUT", "PATCH"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://github.com"
-		},
-		MaxAge: 12 * time.Hour,
-	}))
+
+	router.Use(cors.Default())
 	router.Use(gin.Logger())
 
 	router.GET("/items", listItems(db))
